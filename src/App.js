5a9f0c4e56d08b52b4c6101e693e938ba0f591c8
liya19 <liya1999.19@yandex.ru>
2019-10-29 10:35:18 +0300

    without state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import './App.css';
-import { Layout, Menu, Breadcrumb } from 'antd';
+import {Layout, Menu, Breadcrumb} from 'antd';
 
 function App() {
-    const { Header, Content, Footer } = Layout;
+    const {Header, Content, Footer} = Layout;
+    var ref = {
+        Home: "Home",
+        List: "List",
+        App1: "App1"
+    };
     return (
         <Layout className="layout">
             <Header>
-                <div className="logo" />
+                <div className="logo"/>
                 <Menu
                     theme="dark"
                     mode="horizontal"
                     defaultSelectedKeys={['2']}
-                    style={{ lineHeight: '64px' }}
+                    style={{lineHeight: '64px'}}
                 >
                     <Menu.Item key="1">Test1</Menu.Item>
                 </Menu>
@@ -20,10 +25,14 @@ function App() {
 
             <Content className={"content"}>
                 {/*навигационная цепочка*/}
-                <Breadcrumb style={{ margin: '16px 0' }}>
-                    <Breadcrumb.Item>Home</Breadcrumb.Item>
-                    <Breadcrumb.Item>List</Breadcrumb.Item>
-                    <Breadcrumb.Item>App</Breadcrumb.Item>
+                <Breadcrumb style={{margin: '16px 0'}}>
+                    {
+                        Object.keys(ref).map((oneKey, i) => {
+                            return (
+                                < Breadcrumb.Item key={i}>{ref[oneKey]}</Breadcrumb.Item>
+                            )
+                        })
+                    }
                 </Breadcrumb>
                 <div className={"blockContent"}>Content</div>
             </Content>
@@ -31,4 +40,5 @@ function App() {
         </Layout>
     );
 }
+
 export default App;
