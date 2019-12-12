2c3980ab3f20348f8ff75bcb25c3975648c47618
liya19 <liya1999.19@yandex.ru>
2019-12-13 01:34:08 +0300

    Updates

---
+++ precache-manifest.8ecccff7b3ea0299f671e614d91425cb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddbe761f95da3dd9a906c55ab7b9ee1a",
+    "url": "/github-client/index.html"
+  },
+  {
+    "revision": "46d9e33f996bb0b3478e",
+    "url": "/github-client/static/css/2.83670195.chunk.css"
+  },
+  {
+    "revision": "6c6762327b9f775a2108",
+    "url": "/github-client/static/css/main.53a530a9.chunk.css"
+  },
+  {
+    "revision": "46d9e33f996bb0b3478e",
+    "url": "/github-client/static/js/2.00156159.chunk.js"
+  },
+  {
+    "revision": "6c6762327b9f775a2108",
+    "url": "/github-client/static/js/main.9488bb07.chunk.js"
+  },
+  {
+    "revision": "9815f78edd62dadc619a",
+    "url": "/github-client/static/js/runtime~main.117a3b48.js"
+  },
+  {
+    "revision": "f96a5fbfb901a010426fde0458a9bc01",
+    "url": "/github-client/static/media/main1.f96a5fbf.png"
+  },
+  {
+    "revision": "dcef3abedf0e0761203aaeb85886a6f3",
+    "url": "/github-client/static/media/profile.dcef3abe.jpg"
+  }
+]);
